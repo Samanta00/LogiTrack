22fda24e7c2cb853469d2c4b5786ca93a9b66e27
EllenSamanta <ellen.samanta@outlook.com>
2026-04-02 19:43:47 -0300

    init: adicionando regra de negócio na service : Viagem

---
+++ Api-segura/api/src/main/java/com/api/seguranca/api/viagem/service/ViagemService.java
@@ -0,0 +1,45 @@
+package com.api.seguranca.api.viagem.service;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import com.api.seguranca.api.viagem.entity.ViagemEntity;
+import com.api.seguranca.api.viagem.dto.ViagemDTO;
+import com.api.seguranca.api.viagem.repository.ViagemRepository;
+import com.api.seguranca.api.veiculo.entity.VeiculoEntity;
+import com.api.seguranca.api.veiculo.repository.VeiculoRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ViagemService {
+
+    private final ViagemRepository viagemRepository;
+    private final VeiculoRepository veiculoRepository;
+
+    public ViagemEntity salvar(ViagemDTO dto) {
+
+        if (dto.getVeiculoId() == null) {
+            throw new RuntimeException("veiculoId não pode ser null");
+        };        
+
+        VeiculoEntity veiculo = veiculoRepository.findById(dto.getVeiculoId())
+                .orElseThrow(() -> new RuntimeException("Veículo não encontrado"));
+
+
+        ViagemEntity viagem = new ViagemEntity();
+        viagem.setVeiculo(veiculo);
+        viagem.setDataSaida(dto.getDataSaida());
+        viagem.setDataChegada(dto.getDataChegada());
+        viagem.setOrigem(dto.getOrigem());
+        viagem.setDestino(dto.getDestino());
+        viagem.setKmPercorrida(dto.getKmPercorrida());
+
+        return viagemRepository.save(viagem);
+    }
+
+    public List<ViagemEntity> listar() {
+        return viagemRepository.findAllWithVeiculo();
+    }
+}
