3e4b0129a398a3604b6a77475e6b954dc81316ae
EllenSamanta <ellen.samanta@outlook.com>
2026-05-10 14:59:16 -0300

    lint: melhoria futura para cruar uma rota onde se cria manutenção pelo crud

---
+++ Api-segura/api/src/main/java/com/api/seguranca/api/manutencao/controller/ManutencaoController.java
@@ -0,0 +1,13 @@
+// package com.api.seguranca.api.manutencao.controller;
+
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import lombok.RequiredArgsConstructor;
+
+// @RestController
+// @RequestMapping("/manutencao")
+// @RequiredArgsConstructor
+// public class ManutencaoController {
+    
+// }
